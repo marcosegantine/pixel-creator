869949a09b63cd8db52c61b2157a613ba98d3429
Marcos Segantine <marcosegantine@hotmail.com>
2023-01-31 20:42:57 +0000

    desenvolvido funcionalidades de desfazer e selecionar cor do dot

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,89 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './App.css';
 
 const App = () => {
-function handleClick(event) {
-  
-}
+  const [createDot, setCreateDot] = useState([]);
+  const [dotColor, setDotColor] = useState('#000');
+
+  function handleClick(event) {
+    const newDot = {
+      clientX: event.clientX,
+      clientY: event.clientY,
+      color: dotColor,
+    };
+    setCreateDot((prev) => [...prev, newDot]);
+  }
+
+  function handleUndo(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    setCreateDot(createDot.slice(0, -1));
+  }
+
+  function handleRedo(event) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  function clearWindow(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    setCreateDot([]);
+  }
+
+  function handleDotColor(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    setDotColor(event.target.value);
+  }
 
   return (
-    <div className='container' onClick={handleClick}>
+    <>
+      <form id="colorSelector">
+        <label htmlFor="color">Selecione uma cor</label>
+        <select
+          id="color"
+          type="color"
+          placeholder="escolha a cor"
+          onChange={handleDotColor}
+        >
+          <option value="black">Preto</option>
+          <option value="blue">Azul</option>
+          <option value="yellow">Amarelo</option>
+          <option value="#e113e1">Rosa</option>
+          <option value="red">Vermelho</option>
+          <option value="green">Verde</option>
+          <option value="purple">Roxo</option>
+          <option value="tomato">tomato</option>
+        </select>
+      </form>
 
-      <span style={{height: '10px', width: '10px', background: 'white', display: 'block', borderRadius: '50%'}}></span>
+      <div className="container" onClick={handleClick}>
+        <form id="btns">
+          <button
+            onClick={handleUndo}
+            disabled={createDot.length === 0 ? true : false}
+          >
+            Desfazer
+          </button>
+          <button onClick={handleRedo}>Refazer</button>
+          <button onClick={clearWindow}>Clear</button>
+        </form>
 
-    </div>
-  )
-}
+        {createDot.map((item, index) => (
+          <span
+            key={index}
+            className="dot"
+            style={{
+              left: item.clientX,
+              top: item.clientY,
+              backgroundColor: item.color,
+            }}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
 
-export default App
+export default App;
