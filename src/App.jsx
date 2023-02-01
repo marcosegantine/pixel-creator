f4599dbb3d71bd8350b20754bfa0bf7056c8f840
Marcos Segantine <marcosegantine@hotmail.com>
2023-02-01 14:55:41 +0000

    finalizado projeto com acrescimo de botao para limpar tela, escolher cor do dot e desabilitar botoes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 
 const App = () => {
   const [createDot, setCreateDot] = useState([]);
+  const [undidList, setUndidList] = useState([]);
   const [dotColor, setDotColor] = useState('#000');
 
   function handleClick(event) {
@@ -11,28 +12,34 @@ const App = () => {
       clientY: event.clientY,
       color: dotColor,
     };
-    setCreateDot((prev) => [...prev, newDot]);
+    setCreateDot([...createDot, newDot]);
+    setUndidList([]);
   }
 
   function handleUndo(event) {
-    event.preventDefault();
     event.stopPropagation();
+
+    const gettingLastDot = createDot[createDot.length - 1];
+
+    setUndidList([...undidList, gettingLastDot]);
     setCreateDot(createDot.slice(0, -1));
   }
 
   function handleRedo(event) {
-    event.preventDefault();
     event.stopPropagation();
+
+    const gettingLastDot = undidList[undidList.length - 1];
+
+    setCreateDot([...createDot, gettingLastDot]);
+    setUndidList(undidList.slice(0, -1));
   }
 
   function clearWindow(event) {
-    event.preventDefault();
     event.stopPropagation();
     setCreateDot([]);
   }
 
   function handleDotColor(event) {
-    event.preventDefault();
     event.stopPropagation();
     setDotColor(event.target.value);
   }
@@ -42,32 +49,57 @@ const App = () => {
       <form id="colorSelector">
         <label htmlFor="color">Selecione uma cor</label>
         <select
+          style={{ backgroundColor: dotColor, color: 'white' }}
           id="color"
           type="color"
-          placeholder="escolha a cor"
           onChange={handleDotColor}
         >
-          <option value="black">Preto</option>
-          <option value="blue">Azul</option>
-          <option value="yellow">Amarelo</option>
-          <option value="#e113e1">Rosa</option>
-          <option value="red">Vermelho</option>
-          <option value="green">Verde</option>
-          <option value="purple">Roxo</option>
-          <option value="tomato">tomato</option>
+          <option value="#000000" style={{ background: '#000000' }}>
+            Preto
+          </option>
+          <option value="#11B1EA" style={{ background: '#11B1EA' }}>
+            Azul
+          </option>
+          <option value="#f608f6" style={{ background: '#e113e1' }}>
+            Rosa
+          </option>
+          <option value="#5fee12" style={{ background: '#5fee12' }}>
+            Verde
+          </option>
+          <option value="#e30a47" style={{ background: '#e30a47' }}>
+            Vermelho
+          </option>
+          <option value="#9233be" style={{ background: '#9233be' }}>
+            Roxo
+          </option>
         </select>
       </form>
 
       <div className="container" onClick={handleClick}>
         <form id="btns">
           <button
+            type="button"
             onClick={handleUndo}
             disabled={createDot.length === 0 ? true : false}
           >
             Desfazer
           </button>
-          <button onClick={handleRedo}>Refazer</button>
-          <button onClick={clearWindow}>Clear</button>
+
+          <button
+            type="button"
+            onClick={handleRedo}
+            disabled={undidList.length === 0 ? true : false}
+          >
+            Refazer
+          </button>
+
+          <button
+            type="button"
+            onClick={clearWindow}
+            disabled={createDot.length === 0 ? true : false}
+          >
+            Limpar
+          </button>
         </form>
 
         {createDot.map((item, index) => (
